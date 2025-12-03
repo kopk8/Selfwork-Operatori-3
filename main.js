7e09d1c5abaf1dde4a1a6daa2e723adf8e820882
jose <joseantonionieli25@hotmail.com>
2025-12-03 22:51:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+let num1 = 5;
+
+let num2 = 7;
+
+let somma = num1 + num2;
+
+console.log(somma);
+
+let sottrazione = num1 - num2;
+
+console.log(sottrazione);
+
+let moltiplicazione = num1 * num2;
+
+console.log(moltiplicazione);
+
+let divisione = num1 / num2;
+
+console.log(divisione);
+
+let potenza = num1 ** num2;
+
+console.log(potenza);
+
+console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono: somma ${somma}, sottrazione ${sottrazione}, moltiplicazione ${moltiplicazione}, divisione ${divisione} e potenza ${potenza} `);
+
+
