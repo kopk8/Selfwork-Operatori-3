a2cf9d49e7da423a08b20934771500c0a0c4e229
kopk8 <joseantonionieli25@hotmail.com>
2025-12-03 23:00:05 +0100

    corretto inserimento numeri da parte degli utenti

---
+++ main.js
@@ -0,0 +1,28 @@
+let num1 = parseInt(prompt('inserisci il primo numero'));
+
+let num2 = parseInt(prompt('inserisci il secondo numero'));
+
+
+let somma = num1 + num2;
+
+console.log(somma);
+
+let sottrazione = num1 - num2;
+
+console.log(sottrazione);
+
+let moltiplicazione = num1 * num2;
+
+console.log(moltiplicazione);
+
+let divisione = num1 / num2;
+
+console.log(divisione);
+
+let potenza = num1 ** num2;
+
+console.log(potenza);
+
+console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono: somma ${somma}, sottrazione ${sottrazione}, moltiplicazione ${moltiplicazione}, divisione ${divisione} e potenza ${potenza} `);
+
+
